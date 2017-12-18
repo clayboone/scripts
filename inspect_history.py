3c3e3822e1ef6efb5dbec6d2b9b5d98c919ad26a
Tener Hades <tener.hades@gmail.com>
2017-12-18 17:50:02 -0600

    added cross-platform function
    should return the right directory for the right system with the right separation.

--- inspect_history.py
+++ inspect_history.py
@@ -17,8 +17,8 @@ import sys
 import sqlite3
 import tempfile
 import shutil
-import glob
 import argparse # click not in stdlib
+import platform
 
 # from urllib.parse import urlparse
 from contextlib import contextmanager
@@ -62,11 +62,24 @@ def open_sqlite3(filename, query=None):
     connection.close()
     shutil.rmtree(tmp_filepath)
 
+def get_chrome_userdata_path():
+    """Return this platform's default path to 'User Data' as a string that
+    this platform understands. (eg. On Windows,
+    'C:\\Users\\username\\App Data\\Local\\Google\\Chrome\\User Data')
+    """
+    operating_system = platform.system()[:3]
+    chrome_path_elements = ['Google', 'Chrome', 'User Data']
+
+    if operating_system == 'Win':
+        return os.path.join(os.getenv('LOCALAPPDATA'), *chrome_path_elements)
+    else:
+        raise NotImplementedError
+
+
 def print_history(profile_name, num_rows, outfile=sys.stdout):
     """Read some rows of a chrome history database"""
-    history_filename = os.path.join(
-        os.getenv('LOCALAPPDATA'),
-        *['Google', 'Chrome', 'User Data', profile_name, 'History'])
+    history_filename = os.path.join(get_chrome_userdata_path(),
+                                    *[profile_name, 'History'])
 
     query_string = ('select datetime(last_visit_time/1000000-11644473600,'
                     '"unixepoch"), url from urls order by last_visit_time desc')
@@ -80,11 +93,9 @@ def print_history(profile_name, num_rows, outfile=sys.stdout):
             print(time, data, file=outfile)
 
 def list_chrome_profiles():
-    """List all sub-directories of the the chrome appdata path that contain a
-    a valid History sqlite3 databse filename and print the name of that
-    directory.
+    """List all sub-directories of the chrome 'User Data' path that contain a
+    valid History sqlite3 database and print the profile names to stdout.
     """
-    
     return 0
 
 def main():
