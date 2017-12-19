7821c0192ed7fd122e025c8f62c4bdec84263edf
Tener Hades <tener.hades@gmail.com>
2017-12-18 19:27:34 -0600

    implemented --list-profiles and began --watch

--- inspect_history.py
+++ inspect_history.py
@@ -14,6 +14,7 @@ only the standard library.
 
 import os
 import sys
+import time
 import sqlite3
 import tempfile
 import shutil
@@ -68,19 +69,31 @@ def get_chrome_userdata_path():
     """
     chrome_path_elements = ['Google', 'Chrome', 'User Data']
 
-    if sys.platform.startswith('win'):
+    if sys.platform.startswith('win32') or sys.platform.startswith('cygwin'):
         return os.path.join(os.getenv('LOCALAPPDATA'), *chrome_path_elements)
     else:
-        raise NotImplementedError
+        sys.exit('Platform \'' + sys.platform + '\' is unsupported')
 
+def clear_terminal_screen():
+    """Attempt to clear terminal screen, regardless of platform"""
+    if sys.platform.startswith('win32') or sys.platform.startswith('cygwin'):
+        os.system('cls')
+    elif sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
+        os.system('clear')
+    else: # java
+        print('\n' * shutil.get_terminal_size().lines, end='')
 
-def print_history(profile_name, num_rows, outfile=sys.stdout):
+def print_history(profile_name, num_rows, outfile=sys.stdout,
+                  clear_terminal=False):
     """Read some rows of a chrome history database"""
     history_filename = os.path.join(get_chrome_userdata_path(),
                                     *[profile_name, 'History'])
 
     query_string = ('select datetime(last_visit_time/1000000-11644473600,'
                     '"unixepoch"), url from urls order by last_visit_time desc')
+
+    if clear_terminal:
+        clear_terminal_screen()
 
     with open_sqlite3(history_filename, query=query_string) as cursor:
         # ('2017-9-9 13:20:07', 'https://www.google.com/search?q=hello+world')
@@ -93,8 +106,23 @@ def print_history(profile_name, num_rows, outfile=sys.stdout):
 def list_chrome_profiles():
     """List all sub-directories of the chrome 'User Data' path that contain a
     valid History sqlite3 database and print the profile names to stdout.
+
+    Return: 0 if at least one valid profile was found. 1 otherwise.
     """
-    return 0
+    userdata = get_chrome_userdata_path()
+    found_valid_profile = False
+
+    for element in os.listdir(userdata):
+        if (os.path.isdir(os.path.join(userdata, element)) and
+            os.path.isfile(os.path.join(userdata, *[element, 'History']))):
+            # pylint complains about the spacing above.  According to the
+            # Google Python Style Guide, this is the correct way and pylint
+            # is wrong.
+            print(element)
+            if not found_valid_profile:
+                found_valid_profile = True
+
+    return 0 if found_valid_profile else 1
 
 def main():
     """Program entry point"""
@@ -102,16 +130,23 @@ def main():
     parser = argparse.ArgumentParser(
         description='Inspect your chrome web history')
     parser.add_argument('-l', '--list-profiles',
+                        action='store_true',
+                        default='False',
                         help='List all chrome profile for the current user')
-    parser.add_argument('-p', '--profile', default='Default',
+    parser.add_argument('-p', '--profile',
+                        default='Default',
                         help='the Chrome profile name to inspect')
-    parser.add_argument('-w', '--watch', action='store_true', default='False',
+    parser.add_argument('-w', '--watch',
+                        action='store_true',
+                        default='False',
                         help='Watch file in real-time')
-    parser.add_argument('-n', '--count', type=int, default=10,
+    parser.add_argument('-n', '--count',
+                        type=int,
+                        default=10,
                         help='number of entries to show')
     args = parser.parse_args()
 
-    # --list-profiles supersedes all other options
+    # --list-profiles will preempt other functionality
     if args.list_profiles is True:
         return list_chrome_profiles()
 
@@ -119,16 +154,30 @@ def main():
     if args.watch is True:
         # Attempt to import required module
         try:
-            import watchdog
+            from watchdog.observers import Observer
         except ImportError:
             print('You need to install the watchdog module for this feature')
             print('Try: pip install watchdog')
             return 1
+
         # Configure watchdog to watch our file
+        observer = Observer()
+        observer.schedule(
+            print_history(args.profile, args.count, clear_terminal=True),
+            os.path.join(get_chrome_userdata_path(), args.profile))
 
         # Watch file
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
+
     else:
         print_history(args.profile, args.count)
+
     return 0
 
 if __name__ == '__main__':
