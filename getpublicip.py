9ae81c823e84009d22688e8269a8c31a4ebe60cb
Unknown <tener.hades@gmail.com>
2017-12-20 18:57:05 -0600

    refactor: changed up getpublicip.py quite a bit
    added docstrings, argparse options, main() boilerplate, error checking if no internet access. started support for ipv6 and an --auto feature.

--- getpublicip.py
+++ getpublicip.py
@@ -1,20 +1,65 @@
 #!/usr/bin/env python3
-"""This module finds the outside IP address of the current host"""
+""" get_public_ip.py
+This module finds the outside IP address of the current host.
 
+It's intended to be used as an i3blocks command, so at most this program
+should output exactly one line to stdout.
+
+I've also decided to print 'service unreachable' instead of '0.0.0.0' for both
+IPv4 and IPv6. It's lowercase becaseu the default i3 font doesn't like upper-
+case characters on my screen. And it's english rather than zeros for two rea-
+sons:
+    * 0.0.0.0 is ambiguous because it's the 'any' address in most config files
+    * I don't know the IPv6 equivalent to IPv4's 0.0.0.0; it's probably [::],
+      or somehting similar, which is a far enough departure from a typical-
+      looking v6 address that it doesn't really make sense to have in the i3-
+      blocks status bar area.
+"""
+
+import sys
+import argparse
 import requests
 
-def get_public_ipv4():
+def get_public_ip(version=4):
     """
     Return the outside-global IPv4 address of this host as a string using
-    the HTTPS flavor of httpbin.org. On error, return '0.0.0.0'
+    the HTTPS flavor of httpbin.org. On error, return None.
     """
-    result = '0.0.0.0'
-    request = requests.get('https://httpbin.org/ip')
+    result = None
+
+    try:
+        request = requests.get('https://httpbin.org/ip')
+    except (requests.ConnectionError, requests.ConnectTimeout):
+        pass
+
     if request.status_code == 200:
         for key, val in request.json().items():
             if key == 'origin':
                 result = val
     return result
 
+def main(argv):
+    """Program entry point
+
+    Args:
+        argv (list): Unparsed arguments passed to program
+    """
+    parser = argparse.ArgumentParser(
+        description='Print the public IP address of this host',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    version_group = parser.add_mutually_exclusive_group()
+    version_group.add_argument(
+        '-a', '--auto', action='store_const', const=0, dest='ip_version',
+        default=0,
+        help='')
+    version_group.add_argument(
+        '-4', action='store_const', const=4, dest='ip_version')
+    version_group.add_argument(
+        '-6', action='store_const', const=6, dest='ip_version')
+    args = parser.parse_args(argv)
+
+    print(args)
+    # print('{}'.format(get_public_ip(args.ip_version) or 'service unreachable'))
+
 if __name__ == '__main__':
-    print('{}'.format(get_public_ipv4()))
+    sys.exit(main(sys.argv[1:]))
