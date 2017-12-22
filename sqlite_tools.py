bd0281437bbcc38a353fcd41114dd2b4ed88322b
Tener Hades <tener.hades@gmail.com>
2017-12-22 17:43:22 -0600

    add: new sqlite_tools module

---
+++ sqlite_tools.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""sqlite_tools.py
+
+Accessory functions for working with sqlite3 files and objects in various
+scenarios.
+"""
+
+import sqlite3
+from contextlib import contextmanager
