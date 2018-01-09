e76ec9561241f48cc822213df6eda849d745ea72
Tener Hades <tener.hades@gmail.com>
2018-01-09 04:17:38 -0600

    wip: set up parser

--- get_soma_streams.py
+++ get_soma_streams.py
@@ -2,20 +2,22 @@
 # -*- coding: utf-8 -*-
 """Retrieve SomaFM stream information."""
 
-import os
 import sys
-import json
 import argparse
+import json
 import requests
-
 
 def main(argv):
     """Program entry point when ran."""
     # Parse command line arguments
-    # -v = list more stream info (all stream addresses/qualities, not just the
-    # highest quality, etc)
-    # -s = search? print streams matching regex or something
-    # -q = only print stream address, not name/quality/whatever
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--search', nargs=1)
+    verbose_group = parser.add_mutually_exclusive_group()
+    verbose_group.add_argument('-v', '--verbose', action='store_true',
+                               default=False, help='Print more information')
+    verbose_group.add_argument('-q', '--quiet', action='store_true',
+                               default=False, help='Only print stream address')
+    args = parser.parse_args(argv)
 
     # Retrive list of stations/streams
     # after the class is setup, this line should look like:
@@ -25,7 +27,6 @@ def main(argv):
     # for station in stations:
     #   print(station.get_info())
     # or something.. who knows..
-    pass
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    sys.exit(main(sys.argv[1:]))
