5c6e499f083ff7de6f4a335bdecdd5e5075a339b
Tener Hades <tener.hades@gmail.com>
2018-01-11 01:44:07 -0600

    change: removed unused bits

--- GMouseClicker.py
+++ GMouseClicker.py
@@ -6,19 +6,13 @@ Click the mouse repeatedly until stopped.
 
 Requires:
     https://pypi.python.org/pypi/pynput
-    https://github.com/thesharp/daemonize
 """
 
-import sys
 import time
-
-from daemonize      import Daemonize
 from pynput.mouse   import Button, Controller
 
-def print_stderr(msg):
-    print('{}'.format(msg), file=sys.stderr)
-
 def send_mouse1_event(mouse_down_delay=0.05):
+    """Send a left-click"""
     mouse = Controller()
     mouse.press(Button.left)
     time.sleep(mouse_down_delay)
