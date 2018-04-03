7e530a36e6067105aace80824290f2f57965fd11
Tener Hades <tener.hades@gmail.com>
2018-04-03 02:30:46 -0500

    wip: added a few things to the new one

--- rofi-chrome.sh
+++ rofi-chrome.sh
@@ -2,5 +2,38 @@
 # Copyright Whatever 2018 Tener
 # Officially my first addition to this repo for FreeBSD :)
 
-# Use `rofi` to launch some chrome bookmarks
+x=`cat <<EOF | rofi -dmenu -i -p "Chrome: "
+GMail
+Google Inbox
+Google Calendar
+Reddit
+EOF
+`
+
+case "$x" in
+	"GMail")
+		exec chrome "https://gmail.com"
+		break
+		;;
+	"Google Inbox")
+		exec chrome "https://inbox.google.com"
+		break
+		;;
+	"Google Calendar")
+		exec chrome "https://calendar.google.com"
+		break
+		;;
+	"Reddit")
+		exec chrome "https://reddit.com"
+		break
+		;;
+	"Google Keep")
+		exec chrome "https://keep.google.com"
+		break
+		;;
+	*)
+		# search (for now)
+		echo input was \""$x"\"
+		exec chrome "https://google.com/search?q=$x"
+esac
 
