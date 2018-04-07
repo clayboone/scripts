b5082dbd1cea59a71af0483288e9f28fd8b7d5a2
Tener Hades <tener.hades@gmail.com>
2018-04-07 01:47:22 -0500

    add: ngrok reminer script

---
+++ ngrok.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+# Just tell me I need to go download it
+if __name__ == '__main__':
+    print('Go to ngrok.com, download, extract to ~/.local/bin, and')
+    print('set up `ngrok authtoken <TOKEN>`')
