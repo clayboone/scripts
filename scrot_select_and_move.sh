fe99d1a1adbbca85bc84774d57fc560257ea0fe7
Tener Hades <tener.hades@gmail.com>
2018-04-10 19:47:25 -0500

    change: fix stderr output

--- scrot_select_and_move.sh
+++ scrot_select_and_move.sh
@@ -9,33 +9,33 @@ SCROT=`which scrot`
 XCLIP=`which xclip`
 DESTDIR=/tmp/scrots
 
-if [ ! -x "${SCROT}" ]; then
-	echo "Please install scrot(1)" >2
+fatal() {
+	echo "$@" 1>&2
 	exit 1
+}
+
+if [ ! -x "${SCROT}" ]; then
+	fatal "Please install scrot(1)"
 fi
 
 if [ ! -d "${DESTDIR}" ]; then
-	mkdir -p "${DESTDIR}" || {
-		echo "Failed to create \"${DESTDIR}\". Cowardly aborting." >2
-		exit 1
-	}
+	mkdir -p "${DESTDIR}" || \
+		fatal "Failed to create \"${DESTDIR}\". Cowardly aborting."
 	echo "Created ${DESTDIR}..."
 fi
 
 # Take scrot, and save filename to a variable
-filename=`${SCROT} --select --exec 'echo $f' '%Y-%m-%d-%H%M%S_$wx$h.png'`
+filename=`${SCROT} --select --exec 'echo -n $f' '%Y-%m-%d-%H%M%S_$wx$h.png'`
 
 # Move to a tmpfs/ramdisk
-mv "${filename}" "${DESTDIR}/${filename}" || {
-	echo "Failed to move ${filename} to ${DESTDIR}. Quiting" >2
-	exit 1
-}
+mv "${filename}" "${DESTDIR}/${filename}" || \ 
+	fatal "Failed to move ${filename} to ${DESTDIR}. Quiting"
 
 # And copy it
 if [ -x "${XCLIP}" ]; then
 	cat "${DESTDIR}/${filename}" | xclip -i
 else
-	echo "Unable to locate xclip(1). Skipping." >2
+	echo "Unable to locate xclip(1). Skipping."
 fi
 
 echo "${DESTDIR}/${filename}"
