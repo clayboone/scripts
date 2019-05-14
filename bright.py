30e71fe4be77d80442586c1ffce0e02855cf1c22
Clay Boone <tener.hades@gmail.com>
2019-05-14 15:33:31 +0100

    basic functionality in bright.py

--- bright.py
+++ bright.py
@@ -3,7 +3,6 @@
 import os
 import sys
 import re
-import math
 from enum import Enum
 
 BACKLIGHT_PATH = '/sys/class/backlight/intel_backlight'
@@ -44,9 +43,12 @@ class Backlight(object):
     @brightness.setter
     def brightness(self, value):
         assert isinstance(value, int)
-        assert 100 < value <= self.max_brightness
-
         path = os.path.join(BACKLIGHT_PATH, FILE_BRIGHTNESS)
+
+        if value < 100:
+            value = 100
+        if value > self.max_brightness:
+            value = self.max_brightness
 
         try:
             with open(path, 'w') as fd:
@@ -69,6 +71,7 @@ class CommandLine(object):
 
     def __init__(self):
         self.action = None
+        self.value = None
 
         def shift(x):
             return (x[0], x[1:len(x)])
@@ -76,7 +79,6 @@ class CommandLine(object):
         self.name, args = shift(sys.argv)
 
         while len(args) > 0:
-            #print(f'shifting {args[0]}')
             arg, args = shift(args)
 
             if arg == '-h':
@@ -84,7 +86,23 @@ class CommandLine(object):
                 continue
 
             match = re.search(r'^[+-]\d*%?$', arg)
-            print(match)
+            if match is not None:
+                s = match.string
+                if s[0] == '+':
+                    if s[len(s)-1] == '%':
+                        self.action = Action.INC_PERCENT
+                        self.value = abs(int(s[1:len(s)-1]))
+                    else:
+                        self.action = Action.INC_RAW
+                        self.value = abs(int(s[1:]))
+
+                if s[0] == '-':
+                    if s[len(s)-1] == '%':
+                        self.action = Action.DEC_PERCENT
+                        self.value = abs(int(s[1:len(s)-1]))
+                    else:
+                        self.action = Action.DEC_RAW
+                        self.value = abs(int(s[1:]))
 
     def print_usage(self):
         print(f'Usage: {self.name} [-h] {{<[+-]>NUM[%]}}')
@@ -94,14 +112,23 @@ def main():
     cli = CommandLine()
     backlight = Backlight()
 
+    def percent(val):
+        return int(backlight.max_brightness * val / 100)
+
     if cli.action is None:
         print('{:.0f}%'.format(
             int(backlight.actual_brightness / backlight.max_brightness * 100)
         ))
     elif cli.action is Action.HELP:
         cli.print_usage()
-    else:
-        print('taking action')
+    elif cli.action is Action.INC_PERCENT:
+        backlight.brightness += percent(cli.value)
+    elif cli.action is Action.INC_RAW:
+        backlight.brightness += cli.value
+    elif cli.action is Action.DEC_PERCENT:
+        backlight.brightness -= percent(cli.value)
+    elif cli.action is Action.DEC_RAW:
+        backlight.brightness -= cli.value
 
     #backlight.brightness = 4438
     #print(f'actual = {backlight.actual_brightness}')
