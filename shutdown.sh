6b3569fc378c3fff3588f30f8800812d2766498c
Clay Boone <tener.hades@gmail.com>
2019-07-09 22:54:44 +0100

    more accurate binaries

--- shutdown.sh
+++ shutdown.sh
@@ -2,11 +2,14 @@
 # Display a dialog that a user can cancel. When the dialog is not cancelled,
 # power off the computer. Otherwise, abort.
 
+# arch-linux
+ZENITY=/usr/bin/zenity
+
 for i in `seq 1 100`
 do
     echo $i
     sleep 0.1
-done | zenity --progress \
+done | ${ZENITY} --progress \
               --auto-close \
               --text="Shutting down..." \
               && poweroff
