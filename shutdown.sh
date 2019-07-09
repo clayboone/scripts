dbaf48f1ed65d432dc039f3a1eedeed6963e49c6
Clay Boone <tener.hades@gmail.com>
2019-07-09 22:48:28 +0100

    make shutdown.sh more readable

--- shutdown.sh
+++ shutdown.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-# wait a few seconds and shutdown unless user clicks cancel or hits escape
-for i in `seq 1 100`; do echo $i; sleep 0.1; done | zenity --progress --auto-close --text="Shutting down..." && poweroff
+# Display a dialog that a user can cancel. When the dialog is not cancelled,
+# power off the computer. Otherwise, abort.
+
+for i in `seq 1 100`
+do
+    echo $i
+    sleep 0.1
+done | zenity --progress \
+              --auto-close \
+              --text="Shutting down..." \
+              && poweroff
