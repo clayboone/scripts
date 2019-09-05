271fc9af43753ddba3170ce04966b96ce4c3172b
Clay Boone <tener.hades@gmail.com>
2019-09-05 07:24:43 +0100

    rewrite get_term_info.py

--- get_term_info.py
+++ get_term_info.py
@@ -1,18 +1,7 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""get_term_info.py
+"""Print the size of the terminal."""
 
-Prints some basic terminal information because Windows doesn't show me the
-dimensions of my terminal window when resizing.
-"""
-
-import sys
 import shutil
 
-def main():
-    """Print terminal width and height"""
-    width, height = shutil.get_terminal_size()
-    print('Terminal size: {}x{}'.format(width, height))
-
 if __name__ == '__main__':
-    sys.exit(main())
+    print('{}x{}'.format(*shutil.get_terminal_size()))
