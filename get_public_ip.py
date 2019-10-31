9c44b86d1b93c621ae7c43d58c86a42e07b7f901
Clay Boone <tener.hades@gmail.com>
2019-10-31 23:01:43 +0000

    change up get_public_ip.py

--- get_public_ip.py
+++ get_public_ip.py
@@ -1,95 +1,27 @@
 #!/usr/bin/env python3
-""" get_public_ip.py
-This module finds the outside IP address of the current host.
+"""Print the public IP address of this host to stdout."""
 
-It's intended to be used as an i3blocks command, so at most this program
-should output exactly one line to stdout.
-"""
+import json
 
-import sys
-import argparse
-import socket
-from contextlib import contextmanager
+import requests
 
-@contextmanager
-def open_tcp_connection(host, port, version=6):
-    """Attempt to connect to host on port over TCP.
 
-    Args:
-        host (str): The address of the host to connect to.
-        port (int): The port to connect to on host.
-        version (int): Internet Protocol version to use.
+def main():
+    HTTP_BIN_URI = 'https://httpbin.org/ip'
 
-    Yield (socket.socket) on success, False otherwise
-    """
-    if version == 4:
-        sock = socket.socket(family=socket.AF_INET)
-    else:
-        sock = socket.socket(family=socket.AF_INET6)
+    # FIXME: Legend tells of a time without internet...
+    with requests.get(HTTP_BIN_URI) as response:
+        origin = json.loads(response.content.decode('utf8'))['origin']
 
-    try:
-        sock.connect((host, port))
-    except OSError:
-        sock = False
+    origins = [r.strip() for r in origin.split(',')]
 
-    yield sock
+    # Httpbin returns two identical values in all of my tests. When they change,
+    # it's time to find out why.
+    if any(origins[0] != i for i in origins[1:]):
+        raise ValueError('Values are not the same.')
 
-    if sock:
-        sock.close()
+    print(origins[0])
 
-def get_public_ip(version=6):
-    """Return the outside IP address of this host as a string using the
-    icanhazip.com service.
-
-    Args:
-        version (int): The Internet Protocol version number to find.
-
-    Return (str or None): An IP address string.
-    """
-    with open_tcp_connection('icanhazip.com', 80, version) as sock:
-        if not sock:
-            return None
-        sock.send(b'GET / HTTP/1.1\r\nHost: icanhazip.com\r\n\r\n')
-        message = sock.recv(2048)
-
-    result = str(message, encoding='utf-8').splitlines()[-1]
-    return result
-
-def main(argv):
-    """Program entry point
-
-    Args:
-        argv (list): Unparsed arguments passed to program
-    """
-    parser = argparse.ArgumentParser(
-        description='Print the public IP address of this host',
-        epilog=('If --auto is specified, first attempt to retrieve IPv6 '
-                'address. Then fall back to IPv4 if that fails. If no option '
-                'is specified, --auto is assumed.')
-        )
-    version_group = parser.add_mutually_exclusive_group()
-    version_group.add_argument(
-        '-a', '--auto', action='store_const', const=0, dest='ip_version',
-        default=0)
-    version_group.add_argument(
-        '-4', '--ipv4', action='store_const', const=4, dest='ip_version')
-    version_group.add_argument(
-        '-6', '--ipv6', action='store_const', const=6, dest='ip_version')
-    parser.add_argument(
-        '-f', '--failure_message', type=str, default='service unreachable')
-    args = parser.parse_args(argv)
-
-    if args.ip_version == 0:
-        for version in [6, 4]:
-            address = get_public_ip(version)
-            if address:
-                break
-        else:
-            address = args.failure_message
-    else:
-        address = get_public_ip(args.ip_version) or args.failure_message
-
-    print('{}'.format(address))
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+    main()
