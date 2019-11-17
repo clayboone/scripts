f4cb1d9c1b493d657e50555cbbb3bdec28446089
Clay Boone <tener.hades@gmail.com>
2019-11-17 12:04:14 +0000

    add argparse and logging

--- mykeep.py
+++ mykeep.py
@@ -1,3 +1,5 @@
+import argparse
+import logging
 import os
 from pathlib import Path
 import re
@@ -10,10 +12,13 @@ PASSWORD = ''  # App-password here (remember to delete it in account).
 
 MYKEEP_DIR = Path('/'.join([
     os.getenv('USERPROFILE' if 'win' in sys.platform else 'HOME'),
-    Path(__file__).name.split('.')[0],
-]))
+    Path(__file__).name.split('.')[0]
+]))  # C:\Users\<your_username>\mykeep
+
 MASTER_TOKEN_FILE = MYKEEP_DIR / 'secret_token.txt'
 SAVE_LOCATION_DIR = MYKEEP_DIR / 'notes'
+
+_log = logging.getLogger(__file__)
 
 
 def login_and_sync() -> gkeepapi.Keep:
@@ -22,30 +27,43 @@ def login_and_sync() -> gkeepapi.Keep:
     keep = gkeepapi.Keep()
 
     try:
+        _log.info('Reading credentials from master token file')
         keep.resume(USERNAME, MASTER_TOKEN_FILE.read_text().strip())
     except OSError:
-        # TODO: Consider info if debug or "-v".
         try:
+            _log.info('Falling back to username/password in %s', __file__)
             keep.login(USERNAME, PASSWORD)
         except gkeepapi.exception.LoginException:
-            print('Unable to auth! Figure it out.', file=sys.stderr)
-            sys.exit(1)
+            _log.error('Failed to authenticate with Google')
+            return None
 
         try:
             MASTER_TOKEN_FILE.write_text(keep.getMasterToken())
         except OSError:
-            pass  # TODO: Consider warning if __debug__ or "-v".
+            _log.info('Failed to save token to %s', MASTER_TOKEN_FILE)
 
     keep.sync()
     return keep
 
 
 def main():
-    keep = login_and_sync()
-    num_written = 0
+    parser = argparse.ArgumentParser(
+        description='Download your Google Keep notes')
+    parser.add_argument('-v', '--verbose', action='count', default=0)
+    args = parser.parse_args()
+
+    if args.verbose == 1:
+        logging.basicConfig(level=logging.INFO)
+    if args.verbose >= 2:
+        logging.basicConfig(level=logging.DEBUG)
+
+    if not (keep := login_and_sync()):
+        _log.info('Failed to login and sync with Google Keep')
+        return 1
 
     SAVE_LOCATION_DIR.mkdir(exist_ok=True)
 
+    num_written = 0
     for index, note in enumerate(keep.all()):
         bad_chars = '\?\\\/\:\*\"\<\>\|\r'
         valid_title = re.sub(f'[{bad_chars}]+', ' ', note.title)
