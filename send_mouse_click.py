1611e1ebf6d8a5f366788c297baafa3901707150
Clay Boone <tener.hades@gmail.com>
2019-11-24 14:17:08 +0000

    useless rewrite
    I wanted to use Vim with the Workman layout...

--- send_mouse_click.py
+++ send_mouse_click.py
@@ -6,20 +6,39 @@ Requires:
     https://pypi.python.org/pypi/pynput
 """
 
+import asyncio
+import signal
 import sys
 import time
-from pynput.mouse   import Button, Controller
 
-def send_mouse1_event(mouse_down_delay=0.05):
-    """Send a left-click"""
+from pynput.mouse import Button, Controller
+
+EVENT_LOOP = asyncio.get_event_loop()
+
+
+def click(button=Button.left, duration=0.1):
+    """Send a mouse click."""
     mouse = Controller()
-    mouse.press(Button.left)
-    time.sleep(mouse_down_delay)
-    mouse.release(Button.left)
+    mouse.press(button)
+    time.sleep(duration)
+    mouse.release(button)
 
-def main(argv):
-    send_mouse1_event()
-    return 0
+
+def setup_signal_handlers():
+    def sigint_handler(_signal, _frame):
+        EVENT_LOOP.stop()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sigint_handler)
+
+
+def main():
+    setup_signal_handlers()
+
+    asyncio.ensure_future(click(duration=2))
+    asyncio.ensure_future(click(duration=2))
+    EVENT_LOOP.run_forever()
+
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    main()
