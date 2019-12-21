744223da63944da5d6868fc7a868a4f8b4bbea5f
Clay Boone <tener.hades@gmail.com>
2019-12-21 10:57:20 +0000

    add error checking when offline

--- get_public_ip.py
+++ get_public_ip.py
@@ -6,21 +6,28 @@ import json
 import requests
 
 
-def main():
+def get_public_ip():
     HTTP_BIN_URI = 'https://httpbin.org/ip'
+    ERROR_STRING = '?.?.?.?'
 
-    # FIXME: Legend tells of a time without internet...
-    with requests.get(HTTP_BIN_URI) as response:
-        origin = json.loads(response.content.decode('utf8'))['origin']
+    try:
+        with requests.get(HTTP_BIN_URI) as response:
+            origin = json.loads(response.content.decode('utf8'))['origin']
+    except (requests.ConnectionError, requests.ConnectTimeout) as exc:
+        return ERROR_STRING
 
     origins = [r.strip() for r in origin.split(',')]
 
     # Httpbin returns two identical values in all of my tests. When they change,
     # it's time to find out why.
-    if any(origins[0] != i for i in origins[1:]):
+    if any(origins[0] != origin for origin in origins[1:]):
         raise ValueError('Values are not the same.')
 
-    print(origins[0])
+    return origins[0]
+
+
+def main():
+    print(get_public_ip())
 
 
 if __name__ == '__main__':
