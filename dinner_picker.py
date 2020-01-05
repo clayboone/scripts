530be05594a3de977b23f0337a18e7c630795621
Clay Boone <tener.hades@gmail.com>
2020-01-05 22:51:43 +0000

    refactor variable scopes

--- dinner_picker.py
+++ dinner_picker.py
@@ -10,6 +10,7 @@ from typing import Optional
 class Dinner:
     """Pseudo-random dinner generator."""
 
+    DATE_FMT = "%Y-%m-%d"
     CHANCE_FOR_TAKEOUT = 0.2
 
     DINNERS = {
@@ -43,11 +44,12 @@ class Dinner:
     }
 
     def __init__(self, date: dt.datetime):
-        today = dt.date.today().strftime("%Y-%m-%d")
-        self.date = date.strftime("%Y-%m-%d")
-        self.day_name = "Today" if self.date == today else date.strftime("%A")
-        self.weekday = calendar.weekday(date.year, date.month, date.day)
+        today = dt.date.today().strftime(self.DATE_FMT)
+
         self._choice: Optional[str] = None
+        self._date: dt.datetime = date
+        self.date: str = self._date.strftime(self.DATE_FMT)
+        self.day_name: str = "Today" if self.date == today else date.strftime("%A")
 
         random.seed(self.date)
 
@@ -57,11 +59,13 @@ class Dinner:
     def choose_dinner(self) -> str:
         """Return a random dinner from the apropriate list."""
         if not self._choice:
+            weekday = calendar.weekday(self._date.year, self._date.month, self._date.day)
+
             if self.is_takeout_day():
                 self._choice = "Takeout!"
-            elif self.weekday == calendar.TUESDAY:
+            elif weekday == calendar.TUESDAY:
                 self._choice = random.choice(self.DINNERS["mexican"])
-            elif self.weekday in (calendar.SATURDAY, calendar.SUNDAY):
+            elif weekday in (calendar.SATURDAY, calendar.SUNDAY):
                 self._choice = random.choice(self.DINNERS["hard"])
             else:
                 self._choice = random.choice(self.DINNERS["easy"])
@@ -75,9 +79,10 @@ class Dinner:
         weights = [self.CHANCE_FOR_TAKEOUT, 1.0 - self.CHANCE_FOR_TAKEOUT]
         return random.choices([True, False], weights=weights)[0]
 
+
 def main():
     """Entry point."""
-    today = dt.date.today().strftime("%Y-%m-%d")
+    today = dt.date.today().strftime(Dinner.DATE_FMT)
     dinners = [Dinner(dt.date.today() + dt.timedelta(days=_)) for _ in range(-5, 15)]
 
     for dinner in dinners:
